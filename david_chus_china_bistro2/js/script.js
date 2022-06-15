7a839ccf0a74848048055fc1664aaa8945f77bea
kwhchung <78318019+kwhchung@users.noreply.github.com>
2022-06-15 22:28:33 +0800

    Update script.js

--- david_chus_china_bistro2/js/script.js
+++ david_chus_china_bistro2/js/script.js
@@ -10,10 +10,10 @@ $(function(){
 (function(global){
   var dc = {};
   var homeHtml = "snippets/home-snippet.html";
-  var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+  var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoryHtml = "snippets/category-snippet.html";
-  var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
   var menuItemsTitleHtml = "snippets/menu-items-title.html";
   var menuItemHtml = "snippets/menu-item.html";
 
